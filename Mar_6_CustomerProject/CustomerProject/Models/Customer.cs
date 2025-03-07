8cb19a9f585a8deb1ee271edb97e21e429d3dbf1
harsha07sai <harshasunkara07@gmail.com>
2025-03-07 11:37:42 +0530

    Customer

---
+++ Mar_6_CustomerProject/CustomerProject/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int custId { get; set; }
+        public string? custName { get; set; }
+
+        public string? custUserName { get; set; }
+
+        public string? custPassword { get; set; }
+
+        public string? city { get; set; }
+
+        public string? state { get; set; }
+
+        public string? email { get; set; }
+        public string? mobileNo { get; set; }
+
+    }
+}
