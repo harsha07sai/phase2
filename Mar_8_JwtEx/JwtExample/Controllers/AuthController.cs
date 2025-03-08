58ed30c1794f67a8f9525933abcfb4431ad3f63c
harsha07sai <harshasunkara07@gmail.com>
2025-03-08 12:00:37 +0530

    JWT

---
+++ Mar_8_JwtEx/JwtExample/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using JwtExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtExample.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] JwtExample.Models.LoginRequest request)
+        {
+            var token = await _authService.Authenticate(request.Username, request.Password);
+            if (string.IsNullOrEmpty(token))
+            {
+                return Unauthorized("Invalid credentials");
+            }
+            return Ok(new { Token = token });
+        }
+    }
+}
